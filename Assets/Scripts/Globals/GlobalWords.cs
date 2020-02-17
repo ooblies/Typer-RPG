caba185e81dfb69f06abd74461cb4ac8c461065c
Chris Dudziak <chris.dudziak@gmail.com>
2020-02-17 16:37:50 -0500

    streaming

--- Assets/Scripts/Globals/GlobalWords.cs
+++ Assets/Scripts/Globals/GlobalWords.cs
@@ -3,19 +3,43 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class GlobalWords : Singleton<GlobalWords>
 {
 
-    public string wordPath = "Assets/Words/words.txt";
+    public string wordPath = "words.txt";
     private int maxLength = 0;
 
     public List<string> words = new List<string>();
     
     private void Awake()
     {
-        words = File.ReadAllLines(wordPath).ToList();
+        StartCoroutine(loadWords());
+        
+    }
+
+    private IEnumerator loadWords()
+    {
+        string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, wordPath);
+
+        if (filePath.Contains("://") || filePath.Contains(":///"))
+        {
+            WWW www = new WWW(filePath);
+            yield return www;
+
+            //result = www.text;
+            words = www.text.Split(new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None).ToList();
+            Debug.Log("words loaded remotely");
+        }
+        else
+        {
+            //result = System.IO.File.ReadAllText(filePath);
+            words = File.ReadAllLines("Assets/Words/" + wordPath).ToList();
+            Debug.Log("words loaded locally");
+        }
+
         findMaxLength();
     }
 
