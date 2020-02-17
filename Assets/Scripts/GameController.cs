04838f9c428003043de6d7a0732351f474f2546f
Chris Dudziak <chris.dudziak@gmail.com>
2020-02-17 16:44:27 -0500

    second attempt

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -34,8 +34,6 @@ public class GameController : MonoBehaviour
     }
     private void Awake()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
